7087b635df6e179ba6b0a26fbf08c986ded3e5bf
Lucas Caetano De Macedo <lucas.cmacedo3@senacsp.edu.br>
2024-06-29 09:55:15 -0300

    Estilização do cartaz (com switch)

--- _aula_10/js/script.js
+++ _aula_10/js/script.js
@@ -18,17 +18,70 @@ fetch("https://api.themoviedb.org/3/movie/popular?language=pt-br&page=1&api_key=
     .then((dados) => {
         dados.results.map((filme) => {
             let cartaz = document.createElement("div");
-            cartaz.setAttribute("class", cartaz);
+            cartaz.setAttribute("class", "cartaz");
             let poster = document.createElement("img");
             poster.src = img + filme.poster_path;
             let votos = document.createElement("div")
             votos.setAttribute("class", "votos")
-            votos.innerHTML = filme.vote_average;
+            votos.innerHTML = Math.round(filme.vote_average) + "%";
             let titulo = document.createElement("h2");
             titulo.innerHTML = filme.title;
             let lancamento = document.createElement("p");
-            lancamento.setAttribute("class", "lacamento");
-            lancamento.innerHTML = filme.release_date;
+            lancamento.setAttribute("class", "lancamento");
+            /*
+            Vamos utilizar o comando chamado substring, que
+            nos ajuda a quebrar um string com base na 
+            posição do caracter e a quandidade de caracteres
+            por exemplo 2024-06-29. para pegar os
+            4 primeiros digitosusamos:
+            substring(0,4)  
+            */
+            let ano = filme.release_date.substring(0, 4);
+            let mes = filme.release_date.substring(5, 7);
+            let dia = filme.release_date.substring(8, 10);
+
+            switch (mes) {
+                case "01":
+                    mes = "jan";
+                    break;
+                case "02":
+                    mes = "fev"
+                    break;
+                case "03":
+                    mes = "mar";
+                    break;
+                case "04":
+                    mes = "abr";
+                    break;
+                case "05":
+                    mes = "mai";
+                    break;
+                case "06":
+                    mes = "jun";
+                    break;
+                case "07":
+                    mes = "jul";
+                    break;
+                case "08":
+                    mes = "ago";
+                    break;
+                case "09":
+                    mes = "set";
+                    break;
+                case "10":
+                    mes = "out";
+                    break;
+                case "11":
+                    mes = "nov";
+                    break;
+                default:
+                    mes = "dez";
+                    break;
+            }
+
+
+            lancamento.innerHTML = `dia ${dia} de ${mes} de ${ano}`;
+
 
             cartaz.appendChild(poster);
             cartaz.appendChild(votos);
